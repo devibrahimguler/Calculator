6586f94bb19ba99fe32f3f815d152d29c1821b9e
g.ibrahim.r <gryffindor.ibrahim@icloud.com>
2021-10-01 00:51:54 +0300

    Initial Commit

---
+++ SingletonCalculatorApp/SingletonCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingletonCalculatorAppApp.swift
+//  SingletonCalculatorApp
+//
+//  Created by İbrahim Güler on 1.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SingletonCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
